d9f756a3d80d80cc676913f4fa3226e20bd748c5
Rahul-kr0 <rahulkr76459@gmail.com>
2024-05-08 10:27:00 +0530

    First Commit

--- app/src/main/java/com/example/kotlinlearn/Firstprogram.kt
+++ app/src/main/java/com/example/kotlinlearn/Firstprogram.kt
@@ -4,3 +4,8 @@ package com.example.kotlinlearn
 fun main(){
     println("Hello world")
 }
+
+//In Kotlin:
+//fun is used to declare a function the main() function is where your program starts from the body of a
+//function is written within curly braces {}
+//println() and print() functions print their arguments to standard output

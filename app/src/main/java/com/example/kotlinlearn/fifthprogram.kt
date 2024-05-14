9fe6779af8d514f286f0f39fc8784eaea86d9a6a
Rahul-kr0 <rahulkr76459@gmail.com>
2024-05-14 09:22:19 +0530

    Fifth Commit

--- app/src/main/java/com/example/kotlinlearn/fifthprogram.kt
+++ app/src/main/java/com/example/kotlinlearn/fifthprogram.kt
@@ -1,10 +1,32 @@
 package com.example.kotlinlearn
 
+import androidx.core.app.NotificationCompat.MessagingStyle.Message
+import kotlinx.coroutines.processNextEventInCurrentThread
+import kotlin.math.PI
+
+
+fun main(){
+    hello();
+    println(sum(5,7));
+    println(circle(2));
+    printmsg("Hello")
+}
+
 fun hello(){
     println("Hello Function");
 
 }
 
-fun main(){
-    hello();
+fun sum(x: Int , y: Int): Int
+{
+ return x + y;
 }
+
+fun printmsg(message: String){
+    println(message);
+}
+
+fun circle(radius:Int):Double{
+    return PI*radius*radius;
+}
+
